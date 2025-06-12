86d66a297e5e7006ad5069b3623a34d252b02249
Leo <146775205+LeoAboard@users.noreply.github.com>
2025-06-12 07:56:19 -0400

    Add rota /inscricoes

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/Selecao_Ensino_Medio-main/index.html")
 })
 
+app.get("/inscricoes", function(req, res){
+    res.send("Aqui vamos receber os dados do formulario") 
+})  
+
 app.listen(8081, function(){
     console.log("Servidor operando em: http://localhost:8081/")
 })
